a234b45dd96ed4d3e215c5760367c05d9fa15136
Kevin David Molina Gómez <kevin.molina.go@gmail.com>
2023-10-16 15:18:39 -0600

    initial

--- main.py
+++ main.py
@@ -1,26 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
-def process_string(input_string):
-    # Implement your logic to process the input string
-    # For demonstration purposes, let's assume the processing is converting the string to uppercase
-    processed_string = input_string.upper()
-    return processed_string
-
-
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def homepage():
-    processed_string = None
-
-    if request.method == "POST":
-        input_string = request.form.get("input_string")
-        processed_string = process_string(input_string)
-
-    return render_template("index.html", title="Lenguajes y Automatas II", processed_string=processed_string)
-
-
+    return render_template("index.html", title="INICIO")
+@app.route("/tema1")
+def tema1():
+    return render_template("index.html", title="TEMA 1")
 
 if __name__ == "__main__":
     app.run(debug=True)
