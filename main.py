bc1a1acd6d4e7a07eae0617d424484eb515b305b
Kevin David Molina Gómez <kevin.molina.go@gmail.com>
2023-10-16 14:58:11 -0600

    version2

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
+@app.route("/")
+def homepage():
+    return render_template("index.html", title="HOME PAGE")
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route("/docs")
+def docs():
+    return render_template("index.html", title="docs page")
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+@app.route("/about")
+def about():
+    return render_template("index.html", title="about page")
+
+if __name__ == "__main__":
+    app.run(debug=True)
